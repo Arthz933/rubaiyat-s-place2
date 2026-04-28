77bdb78da6bf4f617c10a7f453c374b992e5f2bb
Arthz933 <arthurassuncao017@gmail.com>
2026-04-28 19:49:09 -0300

    atualização geral

--- main.js
+++ main.js
@@ -1,56 +1,50 @@
 const form = document.getElementById("form")
 const conteudoformulario = document.getElementsByClassName("conteudo-formulario")
 const nomedobarbeiro = document.getElementById("nomedobarbeiro")
-const botaodetarefas = document.getElementById("botaodetarefa")
+const botaodetarefas = document.querySelectorAll(".botaodetarefa")
 const contador = document.getElementById("contador")
 let cliques = 0;
 const listadetarefas = document.getElementById("tarefas")
 const btnzeratarefas = document.getElementById("btnreinicia")
+const espacoresultado = document.getElementById("espaco-resultado")
 
 
-form.addEventListener( "submit", (event) =>  {
-event.preventDefault();
+document.getElementById('btnfecharmodal').addEventListener('click', () => {
+    document.getElementById('modal-alert').style.display = 'none';
+});
 
- checaonomedobarbeiro();
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    checaonomedobarbeiro();
 })
 
 btnzeratarefas.addEventListener("click", () => {
- location.reload();
+    location.reload();
 });
 
-listadetarefas.addEventListener("click", () => {
-    cliques++; 
-    contador.textContent = " olá " + nomedobarbeiro.value + ", você tem " + cliques + " tarefas concluídas !"  // CONSIDERA CLIQUES APENAS NA LISTA DE TAREFAS
-  });
+botaodetarefas.forEach(botao => {
+    botao.addEventListener("click", (event) => {
+        event.preventDefault();
 
+        if(nomedobarbeiro.value == "") {
+            document.getElementById('modal-alert').style.display = 'flex';
+            return;
+        }
 
- function checaonomedobarbeiro() {
-    const barbeiro = nomedobarbeiro.value;
+        cliques++;
+        espacoresultado.textContent = "Resultado:    " + " olá " + nomedobarbeiro.value + ", você tem " + cliques + " tarefas concluídas !";
+    });
+});
 
-
-
-    if(barbeiro == "" && cliques >= 5) {
-       alert('preencha um nome');
+function checaonomedobarbeiro() {
+    if(nomedobarbeiro.value == "") {
+        document.getElementById('modal-alert').style.display = 'flex';
     }
-}  
-
-function funcaoerror(input, message) {
-    const formItem = input.parentElement; 
-    const textMessage = formItem.querySelector("a2")
-
-    textMessage.innerText = message;
-
-    formItem.classname = "conteudo-formulario.error"
-
 }
 
-
-
-
-
-
-
-
-
-
-
+function funcaoerror(input, message) {
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a2")
+    textMessage.innerText = message;
+    formItem.classname = "conteudo-formulario.error"
+}
